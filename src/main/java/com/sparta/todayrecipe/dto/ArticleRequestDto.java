39e3e1b7b561b8dd90bfad66ba3724279d808c94
Code-Angler <xkqkrtk@naver.com>
2021-08-17 00:07:33 +0900

    gitignore

--- src/main/java/com/sparta/todayrecipe/dto/ArticleRequestDto.java
+++ src/main/java/com/sparta/todayrecipe/dto/ArticleRequestDto.java
@@ -1,15 +1,26 @@
 package com.sparta.todayrecipe.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@RequiredArgsConstructor
 public class ArticleRequestDto {
 //    private final String username; // 삭제
     private final String title;
     private final String content;
     private final String imageUrl;
+
+    public ArticleRequestDto(String title, String content, String imageUrl) {
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
 }
